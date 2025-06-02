95ae62e5a4d8906219ce35258beb8af664d2e4b5
EveraldoKVR <everaldo.miguel.silva@gmail.com>
2025-06-02 16:21:24 -0300

    Atividade 2 – Criando subclasses com herança

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-#importando a classe Produtos
-from produto import Produto
+#importando a classe Produto, e as subclasses ProdutoNacional e ProdutoInportado
+from produto import Produto, ProdutoImportado, ProdutoNacional
 
-# passando os valores para os atributos da classe Produto dentro da variavel produto_1
-produto_1 = Produto("Teclado", 100.0, 20)
-# chamando o método exibir detalhes com os atributos inseridos para o produto_1
+# passando os valores para os atributos da classe Produto dentro da variavel produto_1 e exibindo através do método exibir_detalhes
+produto_1 = Produto('Teclado', 100.0, 20)
 produto_1.exibir_detalhes()
+
+# passando os valores para os atributos da subclasse produto nacional e importado e exibindo os detalhes usando o método exibir_detalhes
+produto_nacional_1 = ProdutoNacional('Mouse', 50.0, 15)
+produto_importado_1 = ProdutoImportado('Notebook', 5000.0, 5, 0.15)
+
+produto_nacional_1.exibir_detalhes()
+produto_importado_1.exibir_detalhes()
--- produto.py
+++ produto.py
@@ -7,5 +7,17 @@ class Produto:
         self.estoque = estoque
     # definição do método exibir detalhes 
     def exibir_detalhes(self):
-        print(f"Produto: {self.nome} | Preço: R${self.preco:.2f} | Estoque: {self.estoque} unidades")
+        print(f'\nProduto: {self.nome}\n Preço: R${self.preco:.2f}\n Estoque: {self.estoque} unidades\n' )
+
+# criando a subclasse produto nacional e definindo seus atributos
+class ProdutoNacional(Produto):
+    def __init__(self, nome, preco, estoque):
+        super().__init__(nome, preco, estoque)
+# criando a subclasse produto importado e definindo seus atributos
+class ProdutoImportado(Produto):
+    def __init__(self, nome, preco, estoque, taxa_importacao):
+        super().__init__(nome, preco, estoque)
+        self.taxa_importacao = taxa_importacao
+    def exibir_detalhes(self):
+        print(f'\nProduto: {self.nome}\n Preço: R${self.preco:.2f}\n Estoque: {self.estoque} unidades \nTaxa de importação: {self.taxa_importacao}\n' )
 
